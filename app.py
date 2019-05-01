4611b3799680bbaf586d61c8b38ba59e5986d149
Matthew Griffes <mgriffes@gmail.com>
2019-04-30 19:01:24 -0700

    Updated application file

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    headline = "Working with Flask and Python to develop a web application"
+    return render_template("index.html", headline=headline)
